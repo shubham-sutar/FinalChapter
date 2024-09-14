de2b96a88c0398a82548f68a5f00aea985f7ad65
shubham-sutar <shubhamsutar93@gmail.com>
2024-09-14 10:28:09 +0530

    Code Added

--- Python/UserInpute.py
+++ Python/UserInpute.py
@@ -1,6 +1,14 @@
+import time
+
 fname = input("Enter your fname:")
 lname = input("Enter your lname:")
 
 print(f"{fname} {lname}")
 
+time.sleep(2)
 
+num1 = int(input("enter 1st num:"))
+num2 = int(input("enter 2nd number:"))
+
+result = num1 + num2
+print(f"result is:{result}")
