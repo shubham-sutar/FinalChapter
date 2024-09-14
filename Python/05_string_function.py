917454e2d66ae4975dbf0fe3a9efd467de02bb46
shubham-sutar <shubhamsutar93@gmail.com>
2024-09-14 13:03:43 +0530

    Code Added

---
+++ Python/05_string_function.py
@@ -0,0 +1,12 @@
+# string functions
+
+name = "shubham"
+print(type(name))  # type ---> shows data type
+print(id(name))  # id ---> showing memory address
+print(len(name))  # len ---> length, starts from 1
+
+chara = name.count('h')  # ---> Count the characters
+print(chara)
+
+print(name[2])  # ---> second index character of string i.e "u"
+print(name[-1])  # ---> reverse index "m"
---
+++ Python/06_List.py
@@ -0,0 +1,33 @@
+# list = shopping list
+# milk, butter, chicken
+
+# add items
+# Remove items
+# update items
+# view items
+
+fruit_list = ["mango", "apple", "lemon", "cherry"]
+print(f"my favorite fruits are:{fruit_list}")
+# print(type(fruit_list))
+# print(len(fruit_list))
+# print(fruit_list[3])
+#
+# fruit_list.append("banana")
+# print(fruit_list)
+#
+# fruit_list.extend(["grapes", "jamun"])
+# print(fruit_list)
+#
+# fruit_list.pop()
+# print(fruit_list)
+#
+# fruit_list[0] = "shubham"
+# print(fruit_list)
+
+fruit_list.insert(0, "berry")
+print(fruit_list)
+
+
+
+
+
