2bfdc92f917e3daefbe72c92cc675ffad33c32b9
shubham-sutar <shubhamsutar93@gmail.com>
2024-09-14 16:02:32 +0530

    Code Added

---
+++ Python/07_area_of_circle.py
@@ -0,0 +1,11 @@
+# radius of circle
+# pi value required
+
+
+radius = float(input("enter radius of circle:"))
+pi = 3.14
+area_of_circle = pi * radius * radius
+print(f"The area of circle : {area_of_circle}")
+
+
+
