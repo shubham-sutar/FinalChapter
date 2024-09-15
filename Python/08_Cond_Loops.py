513d00fadcecd02bc487f6f1b2298f5c1038e5ab
shubham-sutar <shubhamsutar93@gmail.com>
2024-09-15 19:52:56 +0530

    Code Added

---
+++ Python/08_Cond_Loops.py
@@ -0,0 +1,34 @@
+# for loop
+'''
+for i in range(0, 11, 1):
+    print(i)
+
+# If else
+
+age = int(input("Enter your age:"))
+
+if age > 18:
+    print("You got entry in club")
+else:
+    print("Your not allowed.")
+
+
+print("Shivani asked to ajay")
+
+print("Do you love me ajay?")
+
+proposal = input("ajay: ")
+while proposal != "yes":
+    proposal = input("Do you love me ajay?\najay: ")
+
+print("thank you")
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i + 1
+'''
+
+
+
