5b9a266544b5eca7047579589c9192a8ed750660
shubham-sutar <shubhamsutar93@gmail.com>
2024-09-27 19:27:31 +0530

    Code Added

---
+++ Python/Learn_with_Mee/04_condi_Loops.py
@@ -0,0 +1,54 @@
+# Conditions and Loops
+
+"""
+# If Else
+# Program for check eligibility of voter by age
+
+age = int(input("enter your age:"))
+if age > 18:
+    print("Your eligible for vote")
+else:
+    print("Your not eligible for vote")
+
+# Elif
+# program for greatest number
+
+num1 = 10
+num2 = 42
+num3 = 78
+
+if num1 > num2:
+    print("number 1 is big")
+elif num2 > num3:
+    print("number 2 is big")
+elif num3 > num1 and num3 > num2:
+    print("number 3 is big")
+else:
+    print("number 3 is small")
+"""
+
+# Loops
+# repeat a block of code multiple times
+"""
+# for loop
+# range(start, stop, step)
+
+for i in range(1, 11, 1):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+    
+# create a list using range
+
+my_list = list(range(1, 5, 1))
+print(my_list)
+"""
+
+"""  
+# While Loop
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+"""
