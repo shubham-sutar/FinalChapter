788a7bec0aaaab4eaa1f592520a1c0185bd53b2c
shubham-sutar <shubhamsutar93@gmail.com>
2024-10-12 23:33:21 +0530

    Code Added

---
+++ Python/Learn_with_Mee/10_oops.py
@@ -0,0 +1,108 @@
+# oops.
+# object-oriented programming
+# Object ?
+# Procedural PL --> Functions
+# Student --> Attributes, behaviour
+"""
+class Person:
+    name = None
+    id = None
+    age = None
+    phone_number = None
+
+    def talk(self):
+        print("I can talk")
+
+    def another(self):
+        print("Im Method")
+
+    def sleep(self, name):
+        print("Im a Method")
+        print("sleep", name)
+
+    def walk(self):
+        print("I am Walking")
+
+
+surya = Person()
+surya.name = "Surya Prakash"
+surya.talk()
+
+sayali = Person()
+sayali.name = "Sayali Sutar"
+sayali.walk()
+"""
+
+# Constructor
+"""
+class Dog:
+    name = None
+    id = None
+
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+    # Constructor ?
+    # Use to initialize the values
+    # of the instance variables (Attributes)
+
+    def bark(self):
+        print("Bhow Bhow")
+
+
+dog1 = Dog()
+dog1.name = "jaymes"
+print(dog1.name)
+
+# dog2 = Dog()
+
+# obj = Dog()
+# obj.bark()
+"""
+
+"""
+class Employee:
+    name = None  # instance variable
+    id = None  # instance variable
+    pin = None  # instance variable
+
+    # Constructor
+    def __init__(self, name, id, pin):
+        self.name = name
+        self.id = id
+        self.pin = pin
+
+    def salary(self):
+        print(f"{Emp_obj1.name} Your salary will be arrives next two days.")
+
+
+Emp_obj1 = Employee("salina", "1001", "4004")
+print(f"{Emp_obj1.name} {Emp_obj1.id} {Emp_obj1.pin}")
+
+Emp_obj1.salary()
+"""
+
+
+class Calc:
+
+    def sum(self, num1, num2):
+        return num1 + num2
+
+    def mult(self, num1, num2):
+        return num1 * num2
+
+    def div(self, num1, num2):
+        return num1 // num2
+
+
+obj1 = Calc()
+
+output = obj1.sum(12, 23)  # sum of numbers
+print(output)
+
+output = obj1.mult(11, 11)  # multiplication
+print(output)
+
+output = obj1.div(120, 20)  # Division
+print(output)
---
+++ Python/Learn_with_Mee/11.oops1.py
@@ -0,0 +1,164 @@
+"""
+class Person:
+    name = "amit"  # instance variable
+    age = None
+
+    def walk(self):
+        id = 1001  # local variable
+        print("I am a simple method")
+        print("Hi", self.age)
+
+
+obj = Person()
+amit.walk()
+"""
+
+"""
+class Car:
+    name = None
+    model = None
+    make = None
+
+    def __init__(self, o_name, o_model, o_make):
+        self.name = o_name
+        self.model = o_model
+        self.make = o_make
+
+    def new_audi(self):
+        print("launching a with name", self.name)
+        print("launched car model is", self.model)
+        print("launched car make in", self.make)
+
+
+obj = Car("Audi", 2022, "india")
+obj.new_audi()
+"""
+
+"""
+class Employee:
+    name = None
+    id = None
+    salary = None
+
+    def __init__(self, f_name, f_id, f_salary):
+        self.name = f_name
+        self.id = f_id
+        self.salary = f_salary
+
+    def emp_info(self):
+        print("Employee name is", self.name)
+        print("Employee id is", self.id)
+        print("Employee salary is", self.salary)
+
+
+obj = Employee("sabrina", 1001, 20000)
+obj.emp_info()
+"""
+
+"""
+class VwoLogin:
+    email = None
+    password = None
+
+    def __init__(self, o_email, o_password):
+        self.email = o_email
+        self.password = o_password
+
+    def login(self):
+        if self.email == "shubhamsutar@gmail.com" and self.password == "shubh123":
+            print("logged in seccessfully")
+        else:
+            print("failed to login")
+
+
+user1 = VwoLogin("shubhamsutar@gmail.com", "shubh123")
+user1.login()  # login
+
+user2 = VwoLogin("sayalisutar@gmail.com", "sayali123")
+user2.login()
+"""
+
+# Encapsulation
+# bind the data variable with the methods
+# Data Member - / class Variables
+# Methods - Def function within the class
+# Wrapping or binding the data variable with the methods - Encapsulation.
+# Hide the data members(class variables, instance variables) by using only the methods.
+"""
+class MyClass:
+
+    def __init__(self):
+        self.name = "Sayali"
+        print(self.name)
+
+    def public_func(self):
+        print("public function")
+
+    def __private_func(self):
+        print("This is private")
+
+    def public_fn_privat(self):
+        self.__private_func()
+
+
+# security --> Not everyone can access your variables/methods
+
+obj1 = MyClass()  # Object creation
+
+obj1.public_func()  # public method call
+
+obj1.public_fn_privat()  # make private callable
+
+obj1.__init__()  # constructor call
+
+# obj1.__private_func() error
+"""
+
+# email and password changed because its Public
+"""
+class VwoLogin:
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def person_login(self):
+        if self.email == "sayalisutar@gmail.com" and self.password == "sayali@123":
+            print("login successfully")
+        else:
+            print("login failed..")
+
+
+obj1 = VwoLogin("sayalisutar@gmail.com", "sayali@123")
+
+obj1.email = "??"  # email = email changed
+obj1.password = "??"  # password = password changed
+obj1.person_login()  # Output = login failed because email, pass changed.
+"""
+
+# email and password Not changed because its Private
+"""
+class VwoLogin:
+    def __init__(self, email, password, name):
+        self.__email = email
+        self.__password = password
+        self.name = name
+
+    def person_login(self):
+        if self.__email == "sayu@gmail.com" and self.__password == "sayu123":
+            print("login successfully")
+        else:
+            print("login failed")
+
+
+obj1 = VwoLogin("sayu@gmail.com", "sayu123", "sayali")
+obj1.__email = "shubham@gmail.com"  # Not changed not access by outsider
+obj1.__password = "shubham123"  # # Not changed not access by outsider
+
+"""
+# print(obj1.name)  accessible because its public. // sayali
+# print(obj1.__email) Not accessible because its private. // error
+"""
+
+obj1.person_login()  # Output = login successfully
+"""
