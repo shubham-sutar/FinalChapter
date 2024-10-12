560bacb71c30baa182006809e5fa825a5b5a7815
shubham-sutar <shubhamsutar93@gmail.com>
2024-10-13 00:35:20 +0530

    Code Added

--- Python/Learn_with_Mee/11.oops1.py
+++ Python/Learn_with_Mee/11.oops1.py
@@ -163,7 +163,7 @@ obj1.__password = "shubham123"  # # Not changed not access by outsider
 obj1.person_login()  # Output = login successfully
 """
 
-
+"""
 class BankAccount:
     def __init__(self):
         self.balance = 0
@@ -210,3 +210,29 @@ if secret_pass == "1234":
     jp_chase.if_you_are_auth(True)
 else:
     jp_chase.if_you_are_auth_user(False)
+"""
+"""
+class Password:
+    def __init__(self, password):
+        self.__password = password
+        self.public_var = 10
+
+    def get_pwd(self, is_auth):
+        if is_auth:
+            print(self.__password)
+        else:
+            print("Invalid password")
+
+    def set_pwd(self, password):
+        if password.endswith("9"):
+            if len(password) > 10:
+                self.__password = password
+                print("Set to correct", self.__password)
+            else:
+                print("Not allowed, weak password")
+
+
+pwd = Password("Hacker123")
+pwd.get_pwd(True)
+pwd.set_pwd("piuytrewqfghj9")
+"""
