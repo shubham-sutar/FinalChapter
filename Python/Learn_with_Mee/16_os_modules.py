ecd917ae4eb327fc44cb031d54eebe07261fbdb3
shubham-sutar <shubhamsutar93@gmail.com>
2024-10-16 11:35:47 +0530

    Code Added

---
+++ Python/Learn_with_Mee/16_os_modules.py
@@ -0,0 +1,10 @@
+# Os Module
+# Os Module use to interact with the operating system
+# get working directory, change dir, file exists, filename, size file, env.
+
+import os
+print(os.name)  # nt
+print(os.getcwd())  # get working directory
+
+
+
