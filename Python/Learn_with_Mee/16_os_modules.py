ca06976b13e511ec5bff9380597e12b688de8616
shubham-sutar <shubhamsutar93@gmail.com>
2024-10-16 13:46:34 +0530

    Code Added

--- Python/Learn_with_Mee/16_os_modules.py
+++ Python/Learn_with_Mee/16_os_modules.py
@@ -6,5 +6,13 @@ import os
 print(os.name)  # nt
 print(os.getcwd())  # get working directory
 
+# os.mkdir("TextFiles")
+size = os.path.getsize('sample.txt')
+print(size)
+
+# list all directories
+all_dir = os.listdir(r"C:\Users\Admin\PycharmProjects\FinalChapter\Python\Learn_with_Mee")
+print(all_dir)
+
 
 
