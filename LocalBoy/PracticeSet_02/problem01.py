ec222d511996b659690ccc6a4e65835a0bd5901b
shubham-sutar <shubhamsutar93@gmail.com>
2025-03-12 22:48:56 +0530

    Code Added

---
+++ LocalBoy/PracticeSet_02/problem01.py
@@ -0,0 +1,8 @@
+# Problem 01
+# write a program to add two numbers.
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+result = num1 + num2
+
+print(result)
---
+++ LocalBoy/PracticeSet_02/problem02.py
@@ -0,0 +1,10 @@
+# Problem 02
+# write a python program to find remainder when a number is divided by z.
+
+a = 7
+z = 3
+
+remainder = a % z
+print("remainder is : ", remainder)
+
+
