9bf6dbbac62430a2e36de3468567975ae1a82cbd
shubham-sutar <shubhamsutar93@gmail.com>
2025-03-13 22:04:29 +0530

    Code Added

---
+++ LocalBoy/01_Basics/11_list.py
@@ -0,0 +1,14 @@
+# List
+
+friends = ["shubham", "sayali", "Orange", "False", "integer"]
+
+print(friends[0])
+friends[2] = "loves"
+print(friends)
+print(friends[1:3])
+
+# Methods
+
+# Insertion [.append]
+
+
