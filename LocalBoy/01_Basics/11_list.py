d5afbd90c284a6d069db51c27a2f8922cb1695b5
shubham-sutar <shubhamsutar93@gmail.com>
2025-03-14 10:21:12 +0530

    Code Added

--- LocalBoy/01_Basics/11_list.py
+++ LocalBoy/01_Basics/11_list.py
@@ -11,4 +11,29 @@ print(friends[1:3])
 
 # Insertion [.append]
 
+friends.append("bheem")
+print(friends)  # add bheem to end of the list(at the end)
+
+# insertion [.insert]
+digits = [13, 11, 1, 5, 8]
+digits.insert(3, 99)
+print(digits)  # added 99 at 3 index
+
+# Sort
+
+friends.sort()
+print(friends)
+
+friends[0] = "false"
+print(friends)
+
+friends.sort()
+print(friends)
+
+# reverse
+cars = ["mustang", "pagani", "ds", "dart", "vulcan"]
+cars.reverse()
+print(cars)
+
+
 
