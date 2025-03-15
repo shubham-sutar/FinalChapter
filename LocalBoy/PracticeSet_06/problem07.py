a1c98c5f9c94ebba6a1fa30d05fac5693c0e7d94
shubham-sutar <shubhamsutar93@gmail.com>
2025-03-16 00:14:58 +0530

    Code Added

--- LocalBoy/PracticeSet_06/problem07.py
+++ LocalBoy/PracticeSet_06/problem07.py
@@ -1,2 +1,10 @@
-# problem 06
-# 6.
+# problem 07
+# 7. Write a program to find out whether is post is talking about Harry or not.
+
+user_post = input("Write a post: ")
+
+if "harry" in user_post:
+    print("Post is talking about harry")
+else:
+    print("post is not talking about harry.")
+
