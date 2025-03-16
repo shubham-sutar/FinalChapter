20ee48ebb101f115990093fc73b58e061f35bf56
shubham-sutar <shubhamsutar93@gmail.com>
2025-03-16 11:59:04 +0530

    Code Added

---
+++ LocalBoy/01_Basics/20_break_&_continue.py
@@ -0,0 +1,17 @@
+# break statement
+"""
+for items in range(1, 100):
+    if items == 55:
+        break
+    print(items)
+print("break operation")
+
+"""
+
+# continue
+for item in range(0, 11):
+    if item == 5:
+        continue  # skip the iteration
+    print(item)
+
+
---
+++ LocalBoy/01_Basics/21_pass.py
@@ -0,0 +1,9 @@
+# Pass in for loop
+
+for i in range(0, 600):
+    pass
+
+i = 0
+while i <= 10:
+    print(i)
+    i = i + 1
