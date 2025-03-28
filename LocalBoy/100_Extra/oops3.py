9ee75e6e04a6f6f4ecead7c45bb41f2671ba336b
shubham-sutar <shubhamsutar93@gmail.com>
2025-03-28 23:10:52 +0530

    Code Added

--- LocalBoy/100_Extra/oops3.py
+++ LocalBoy/100_Extra/oops3.py
@@ -2,28 +2,67 @@
 
 # Single Inheritance
 
-class Father:
-    key = "abc@123"
-
-    def father_method(self):
-        return "father method"
-
-    @staticmethod
-    def stats():
-        print("hi father.")
-
-
-class Child(Father):
-
-    def child_method(self):
-        return "child_methid"
+# class Father:
+#     key = "abc@123"
+#
+#     def father_method(self):
+#         return "father method"
+#
+#     @staticmethod
+#     def stats():
+#         print("hi father.")
 
 
-obj = Child()
-output = obj.child_method()
-print(output)
+# class Child(Father):
+#
+#     def child_method(self):
+#         return "child_method"
+#
+#
+# obj = Child()
+# output = obj.child_method()
+# print(output)
+#
+# output1 = obj.father_method()
+# print(output1)
+#
+# Father.stats()  # call the static method
 
-output1 = obj.father_method()
-print(output1)
+class MyClass:
 
-Father.stats()  # call the static method
+    @property
+    def name(self):
+        return f"my name is {self.fname}"
+
+    @name.setter
+    def name(self, value):
+        self.fname = value.split(" ")[0]
+
+    @property
+    def salary(self):
+        return f"{self.esalary}"
+
+    @salary.setter
+    def salary(self, value):
+        self.esalary = value
+
+    @property
+    def mobile_num(self):
+        return f"{imobile_num}"
+
+    @mobile_num.setter
+    def mobile_num(self, value):
+        self.imobile_num = value
+
+
+obj9 = MyClass()
+obj9.imobile_num = "9156364230"
+print(obj9.imobile_num)
+
+obj1 = MyClass()
+obj1.esalary = "15000"
+print(obj1.esalary)
+
+obj = MyClass()
+obj.fname = "shubham sutar"
+print(obj.fname)
---
+++ LocalBoy/project02_prft_guess/main.py
@@ -0,0 +1,16 @@
+# perfect guess number.
+import random
+from random import randint
+
+num = random.randint(1, 100)
+a = -1
+guesses = 0
+while a != num:
+    guesses += 1
+    a = int(input("Guess the number: "))
+    if a > num:
+       print("lower number please: ")
+    else:
+        print("Highr number please")
+
+print(f"you have guesed number:{num}")
