104d9cc75c65fb60514ee4cd2984bf99bdb1525c
shubham-sutar <shubhamsutar93@gmail.com>
2025-10-01 23:20:18 +0530

    Code Added

--- LocalBoy/NewChapter/variables.py
+++ LocalBoy/NewChapter/variables.py
@@ -1,56 +1,73 @@
-import pyttsx3
-import os
-import pyjokes
-
 """
-import pyjokes
+num1 = 14
+num2 = 16
 
-print("Hello World")
+sum = num1 + num2
+print("addition of two numbers is sum", sum)
 
-joke = pyjokes.get_joke()
-print(joke)
 
-# pyjokes is module
+num3 = 10
+num4 = 10
 
-# Read Evaluate print loop = repl
+sum1 = num3 + num4
+print(sum1)
 
-print("----------- Printing Jokes------------------")
+bul = (45 > sum and 10 > sum1)
+print(bul)
 
-joke = pyjokes.get_joke()
-print(joke)
+# TYpe casting
+
+num1 = 34.6
+num2 = 11.9
+
+sum = int(num1 + num2)
+print(f"Type casting float to int = {sum}")
+
+
+num1 = "45.6"
+num2 = float(num1)
+t = type(num2)
+print(t)
+
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+
+sum = num1 + num2
+print(f"by taking as user input the sum is = {sum}")
+print(f"by taking as user input the sum is = {sum}")
+
+num = int(input("enter any num: "))
+z = 2
+
+remainder = num % z
+print(f"the remainder is {remainder}")
+
+num = input("Enter any number: ")
+print(type(num))
+
+a = 34
+b = 80
+
+if a > b:
+    print("a is greater num")
+else:
+    print("b is grater num")
+
+num1 = int(input("enter num1 : "))
+num2 = int(input("enter num2 : "))
+num3 = int(input("enter num3 : "))
+
+Total = num1 + num2 + num3
+
+avg = int(Total / 2)
+print(f"The Average of numbers is {avg} ")
+
+
+num = int(input("Enter a number: "))
+sqr = num**2
+print(f"The sqr of number enter by user is {sqr}")
 """
-star = "shubham"
 
-# Practice Set 1
-print(f'''Twinkle twinkle little {star}.
-How I wonder what you are.
-Up above the world so high.
-Like a diamond in the sky.
-Twinkle twinkle little star.
-How I wonder what you are.
-
-Twinkle twinkle little star.
-How I wonder what you are.
-Up above the world so high.
-Like a diamond in the sky.
-Twinkle twinkle little star.
-How I wonder what you are.''')
-
-# speech = pyttsx3.speak("I will call you back")
-# print(speech)
-
-# engine = pyttsx3.init()
-# engine.say("stop")
-# engine.runAndWait()
-
-directory_path = r"F:\Tutorials"
-content = os.listdir(directory_path)
-print(content)
-
-joke = pyjokes.get_joke()
-print(joke)
-
-
-
+# 1:22
 
 
