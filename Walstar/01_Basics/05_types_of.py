b8a962a5f7d7feaa88e964a82b5e6d9f10222a9b
shubham-sutar <shubhamsutar93@gmail.com>
2025-10-01 19:17:49 +0530

    Code Added

---
+++ Walstar/01_Basics/05_types_of.py
@@ -0,0 +1,33 @@
+print("hello world")
+
+x = 10
+y = 10
+
+print(x + y)
+
+# this is a comment in python
+"""
+this is a multiline comment
+"""
+
+# this is a multiline comment
+# this is a multiline comment
+# this is a multiline comment
+# this is a multiline comment
+
+import keyword
+
+print(keyword.kwlist)
+
+num1 = 16
+num2 = 14
+
+print(num1 + num2)
+
+name1 = "shubham"
+luck_num = 8
+
+print(name1, luck_num)
+
+print(type(luck_num))
+
