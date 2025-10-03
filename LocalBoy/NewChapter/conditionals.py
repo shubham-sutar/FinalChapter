6315f05068396c6b2a78f9a81108141f44b83990
shubham-sutar <shubhamsutar93@gmail.com>
2025-10-03 22:39:33 +0530

    Code Added

--- LocalBoy/NewChapter/conditionals.py
+++ LocalBoy/NewChapter/conditionals.py
@@ -1,5 +1,5 @@
 # Conditionals
-
+"""
 age = int(input("enter your age: "))
 
 if age > 18:
@@ -10,3 +10,31 @@ else:
     print("enter your not eligible for vote.")
 
 print("Program End")
+
+age = input("your age is greater than 18 type yes or no: ")
+
+if age == "yes":
+    print("your eligible for vote")
+else:
+    print("your not eligible")
+
+print("Program End")
+
+msg1 = "make a lot of money"
+msg2 = "buy now"
+msg3 = "subscribe this"
+msg4 = "click this"
+
+message = input("enter your comment here: ")
+if (msg1 in message) or (msg2 in message) or (msg3 in message) or (msg4 in message):
+    print("Spam msg detected")
+else:
+    print("msg not under spam")
+"""
+
+username = input("Enter the username: ")
+
+if (len(username)) < 10:
+    print("username requires more than 10 letters")
+else:
+    print("All is well")
